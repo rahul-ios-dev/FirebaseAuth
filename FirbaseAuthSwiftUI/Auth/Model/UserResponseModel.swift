952344a3567f65ff0f5d27cb90e39b06c2c95821
rahul-ios-dev <rahul.acharya91519@gmail.com>
2024-11-28 17:08:50 +0530

    commit

---
+++ FirbaseAuthSwiftUI/Auth/Model/UserResponseModel.swift
@@ -0,0 +1,23 @@
+//
+//  UserResponseModel.swift
+//  FirbaseAuthSwiftUI
+//
+//  Created by Rahul Acharya on 28/11/24.
+//
+
+import Foundation
+
+struct UserResponseModel {
+    let name: String
+    let email: String
+    let password: String
+    let id: Int
+    let createdAt: String
+    let updatedAt: String
+    let isActive: Bool
+    let isAdmin: Bool
+    let isDeleted: Bool
+    let role: String
+    let token: String
+    let avatar: String
+}
--- Pods/Target Support Files/Pods-FirbaseAuthSwiftUI/Pods-FirbaseAuthSwiftUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FirbaseAuthSwiftUI : NSObject
-@end
-@implementation PodsDummy_Pods_FirbaseAuthSwiftUI
-@end
